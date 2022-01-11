0e53f717241cad1d1847771700c4df6c185140f4
Joonatan Lipiäinen <joonatan.a.lipiainen@gmail.com>
2022-01-11 12:05:31 +0200

    Oliotehtava commit

---
+++ harjoitus1auto/harjoitus1auto/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace harjoitus1auto
+{
+   
+       class Ajoneuvo
+
+    {
+
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+        public void Tulostadata()
+        {
+            Console.WriteLine("Ajoneuvo: ");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Nopeus: " + Nopeus);
+            Console.WriteLine("-- Renkaat: " + Renkaat);
+        }
+        // public string ToString()
+        //{
+        //  }
+    }
+}
+
---
+++ harjoitus1auto/harjoitus1auto/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace harjoitus1auto
+{
+    class Program
+    { 
+      static void Main(string[] args)
+    {
+        // Olio Ajoneuvo =luokasta
+        Ajoneuvo auto = new Ajoneuvo();
+
+        // Asetetaan oliolle "auto" arvot
+        auto.Nimi = "Toyota";
+        auto.Nopeus = 160;
+        auto.Renkaat = 4;
+
+        // Käytetään "auto" olion toimintoja
+        auto.Tulostadata();
+        string autonTiedot = auto.ToString();
+        Console.WriteLine(autonTiedot);
+
+        // Luodaan toinen olio luokasta Ajoneuvo
+
+        Ajoneuvo mopo = new Ajoneuvo();
+        mopo.Nimi = "Skootteri";
+        mopo.Nopeus = 55;
+        mopo.Renkaat = 2;
+
+        mopo.Tulostadata();
+        Console.WriteLine(mopo.ToString());
+
+
+    }
+}
+}
+       
+
