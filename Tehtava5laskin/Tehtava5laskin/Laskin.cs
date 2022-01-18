85582e5fe2f3a4034c31a6a7575ce580b7b54842
Joonatan Lipiäinen <joonatan.a.lipiainen@gmail.com>
2022-01-18 12:20:50 +0200

    Commit

---
+++ Tehtava5laskin/Tehtava5laskin/Laskin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tehtava5laskin
+{
+   static class Laskin
+    {
+        public static float Summa(float a, float b)
+        {
+            return a + b;           
+        }
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
