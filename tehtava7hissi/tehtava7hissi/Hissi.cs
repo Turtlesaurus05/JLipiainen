d3200cae5913f2b280cd9bc2fc8e23171120b281
Joonatan Lipiäinen <joonatan.a.lipiainen@gmail.com>
2022-01-31 09:43:12 +0200

    commit

---
+++ tehtava7hissi/tehtava7hissi/Hissi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tehtava7hissi
+{
+    class Hissi
+    {
+        private int kerros;
+
+
+        public int Kerros
+        {
+            get => kerros;
+            set
+            { 
+   
+
+             if (value > 6)
+                {
+                    Console.WriteLine("Annettu kerros oli liian suuri.");
+                    value = 6;
+
+                }
+                 else if (value< 1 )
+                {
+                    Console.WriteLine("Annetu kerros oli liian pieni.");
+                    value = 1;
+
+                }
+                       Console.WriteLine("Kerros on asetettu. " + value);
+                kerros = value;
+            }
+
+        }
+
+       
+    }
+}
+    
+
+          
---
+++ tehtava7hissi/tehtava7hissi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace tehtava7hissi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uusikerros;
+
+            Console.WriteLine("Kerros jonne haluat.");
+            Hissi hissi = new Hissi();
+            Console.WriteLine("Kerros luotu, Jatka kirjoittamalla Jatka.");
+
+            while (true)
+            {
+                Console.WriteLine("Kirjoita uusi kerros (1-6) > ");
+                string luettuarvo = Console.ReadLine();
+
+                if (luettuarvo.Equals("Sammuta"))
+                {
+                    break;
+                }
+                bool result = int.TryParse(luettuarvo, out uusikerros);
+
+                if (result)
+                {
+                    hissi.Kerros = uusikerros;
+
+                }
+                else
+                {
+                    Console.WriteLine("VIRHE! Annettu kerros on virheellinen. Koita syöttää uusi kerros.");
+                }
+            }
+            Console.WriteLine("Olet poistunut nyt hissistä.");
+          
+        }
+    }
+}
