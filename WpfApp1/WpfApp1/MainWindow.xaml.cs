92aeb5a6c406ddf59760449ddbcfa0eb9ab2142b
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2022-04-26 12:34:00 +0300

    Loppytyö

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -30,23 +30,23 @@ namespace WpfApp1
 
             if (cbKauhu.IsChecked == true)
             {
-                tbGames.Text = tbGames.Text + tbGamename.Text + " (Kauhu)  ";
+                tbKirjat.Text = tbKirjat.Text + tbGamename.Text + " (Kauhu)  ";
             }
             if (cbRomantiikka.IsChecked == true)
             {
-                tbGames.Text = tbGames.Text + tbGamename.Text + " (Romantiikka)  ";
+                tbKirjat.Text = tbKirjat.Text + tbGamename.Text + " (Romantiikka)  ";
             }
             if (cbFantasia.IsChecked == true)
             {
-                tbGames.Text = tbGames.Text + tbGamename.Text + " (Fantasia)  ";
+                tbKirjat.Text = tbKirjat.Text + tbGamename.Text + " (Fantasia)  ";
             }
             if (cbScifi.IsChecked == true)
             {
-                tbGames.Text = tbGames.Text + tbGamename.Text + " (Scifi)  ";
+                tbKirjat.Text = tbKirjat.Text + tbGamename.Text + " (Scifi)  ";
             }
 
         }
-
+        //Tämä kohta on se jonka pitäisi siirtää tekstin toiseen tiedostoon. Ja kun syötät ne kertoo onko syötetty vai eikö?
         private void btnTotext_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -55,41 +55,41 @@ namespace WpfApp1
                 string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 using (StreamWriter file = new StreamWriter(System.IO.Path.Combine(docPath, "GameList.txt")))
                 {
-                    file.WriteLine(tbGames.Text);
+                    file.WriteLine(tbKirjat.Text);
                 }
-                tbSuccessful.Text = "Action Completed Successfully";
-                tbGames.Text = string.Empty;
+                tbSuccessful.Text = "Kirjat on syötetty nyt.";
+                tbKirjat.Text = string.Empty;
             }
-            //jos tule virhe siita ilmoitetaan
+           
             catch
             {
                 tbSuccessful.Text = string.Empty;
                 tbSuccessful.Text = "Error";
-                tbGames.Text = string.Empty;
+                tbKirjat.Text = string.Empty;
 
             }
 
 
         }
-
-        private void cbShooter_Checked(object sender, RoutedEventArgs e)
+        //Tässä kohtaan luetellaan eri genret. Elikkä kun painat vaikka kauhu genren se menee "kirjan perään"
+        private void cbKauhu_Checked(object sender, RoutedEventArgs e)
         {
-            tbGenreinfo.Text = "Kauhukirjallisuus on kirjallisuuden laji, joka käsittelee kauhua herättäviä asioita tai olentoja. Usein tarkoituksena on herättää lukijassa samaa kauhun ...";
+           
         }
 
-        private void cbIndie_Checked(object sender, RoutedEventArgs e)
+        private void cbRomantiikka_Checked(object sender, RoutedEventArgs e)
         {
-            tbGenreinfo.Text = "Romantiikka (kirjallisuus) ... Romantiikka on idealistinen ja tunne-elämää korostava kirjallisuuden suuntaus erityisesti 1700–1800-lukujen vaihteessa Saksassa. ";
+          
         }
 
-        private void cbRPG_Checked(object sender, RoutedEventArgs e)
+        private void cbFantasia_Checked(object sender, RoutedEventArgs e)
         {
-            tbGenreinfo.Text = "Fantasiakirjallisuus on kaunokirjallisuuden lajityyppi, johon kuuluu olennaisena osana fantasian elementtejä, ihmeellisiä, yliluonnollisia tai mahdottomia asioita.";
+          
         }
 
-        private void cbMOBA_Checked(object sender, RoutedEventArgs e)
+        private void cbScifi_Checked(object sender, RoutedEventArgs e)
         {
-            tbGenreinfo.Text = "Tieteiskirjallisuus eli science fiction -kirjallisuus on kirjallisuudenlaji, joka käsittelee tieteen ja tekniikan vaikutusta ihmisiin ja koko yhteiskuntaan. ";
+        
         }
 
         private void Off_Click(object sender, RoutedEventArgs e)
